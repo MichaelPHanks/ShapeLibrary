91d05c0e7c665cd5cba10a11cde385a8f2d794bf
Dingleberryipod <a02330716@usu.edu>
2024-06-27 22:44:52 -0600

    added test files, probably going to do more tweaking to overall structure

---
+++ src/test/kotlin/PointTest.kt
@@ -0,0 +1,32 @@
+import org.example.Point
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PointTest
+{
+    @Test
+    fun testPointCreation()
+    {
+        val point1 = Point(1.0,1.2)
+        assertEquals(1.0, point1.getX())
+    }
+
+    @Test
+    fun testPointMoveX()
+    {
+
+    }
+
+    @Test
+    fun testPointMoveY()
+    {
+
+    }
+
+    @Test
+    fun testPointClone()
+    {
+
+    }
+
+}
---
+++ src/test/kotlin/RectangleTest.kt
@@ -0,0 +1,35 @@
+import org.example.Point
+import org.example.Rectangle
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RectangleTest
+{
+    @Test
+    fun testRectangleCreation()
+    {
+        val point1 = Point(1.0,2.0)
+        val point2 = Point(-1.2,6.4)
+        val rectangle1 = Rectangle(point1, point2)
+        //assertEquals(rect)
+    }
+
+    @Test
+    fun testRectangleMoveX()
+    {
+
+    }
+
+    @Test
+    fun testRectangleMoveY()
+    {
+
+    }
+
+    @Test
+    fun testRectangleClone()
+    {
+
+    }
+
+}
